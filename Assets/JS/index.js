36703a520b7e12ea39593e4d584b2f7903ef0ee3
Joseph Guajardo <josephguajardo@ymail.com>
2021-02-17 20:45:35 -0800

    adding JavaScript, css, and HTML

---
+++ Assets/JS/index.js
@@ -0,0 +1,32 @@
+
+
+function getApi() {
+    //url for GitHub
+    var requestApi = 'https://api.github.com/users/jagg1991/repos?type,page,per_page,sort'
+
+    fetch(requestApi)
+        .then(function (response) {
+            return response.json();
+        })
+        //getting data from GitHub 
+        .then(function (data) {
+            console.log(data)
+            //for loop to get data for respitories
+            for (i = 0; i < data.length; i++) {
+
+                console.log(data[i].html_url);
+                button = $('<button>').addClass('results btn btn-secondary').attr('url', data[i].html_url).text(data[i].name)
+                $('#resp').append(button)
+
+
+
+            }
+            //adding an onclick function for buttons created 
+            $("#resp").on("click", "button", function () {
+                window.location = ($(this).attr("url"));
+            });
+
+        })
+}
+
+getApi();
